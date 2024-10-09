e762ce53f3f5adcc422e33ab40d7ffcd25a40966
San_Analytics <sansuganyas@outlook.com>
2024-10-10 04:41:35 +0530

    File optimize

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pickle
 from datetime import date, timedelta
 from streamlit_option_menu import option_menu
 from scipy.special import inv_boxcox
-
+import warnings
 
 # Set page configuration
 st.set_page_config(
@@ -34,6 +34,7 @@ st.markdown("""
     div.stTabs [data-baseweb="tab-list"] button[aria-selected="true"] {
         background-color: #009688;
         color: white;
+    }
 
     div.stTabs [data-baseweb="tab-list"] button:hover {
         background-color: #3e8e41;
@@ -89,10 +90,19 @@ with open(r'pkls/ADA_Classifier.pkl', 'rb') as f:
 
 with open(r'pkls/GB_Regressor.pkl', 'rb') as f:
     Reg_model = pickle.load(f)
+    
+    
+if 'current_page' not in st.session_state:
+    st.session_state.current_page = 'About'
+if 'data' not in st.session_state:
+    st.session_state.data = None
+if 'prediction' not in st.session_state:
+    st.session_state.prediction = None
+if 'should_navigate' not in st.session_state:
+    st.session_state.should_navigate = False
  
 
-
-
+    
 with st.sidebar:
     st.markdown("<hr style='border: 2px solid #ffffff;'>", unsafe_allow_html=True)
     
@@ -101,6 +111,7 @@ with st.sidebar:
         icons=['house-door-fill', 'bar-chart-fill'],
         menu_icon="cast",
         default_index=0,
+        key='menu_option',
         styles={
             "container": {"padding": "12!important", "background-color": "gray"},
             "icon": {"color": "#000000", "font-size": "25px", "font-family": "Times New Roman"},
@@ -110,7 +121,10 @@ with st.sidebar:
     )
     st.markdown("<hr style='border: 2px solid #ffffff;'>", unsafe_allow_html=True)
 
-
+if st.session_state.should_navigate:
+    selected = 'Customer Insights'
+    st.session_state.should_navigate = False
+    
 st.markdown("<h1 style='text-align: center; font-size: 38px; color: #808080; font-weight: 700; font-family: inherit;'>Insurance Analytics & Insights</h1>", unsafe_allow_html=True)
 st.markdown("<h4 style='text-align: center; font-size: 26px; color: #808080; font-weight: 200; font-family: inherit;'>Data-Driven Solutions for Customer Segmentation and Fraud Detection</h4>", unsafe_allow_html=True)
 
@@ -151,7 +165,8 @@ if selected == "About":
         Contributions to this project are welcome. If you find any issues or have suggestions for improvements, please open an issue or submit a pull request in the <a href="{}">GitHub Repository</a>.
     </p>""".format(github_url), unsafe_allow_html=True)
 
-if selected == "Customer Profile Input":
+elif selected == "Customer Profile Input":
+    st.title("Customer Profile")
 
 
     # Options for various dropdowns
@@ -192,79 +207,25 @@ if selected == "Customer Profile Input":
         incident_type = st.selectbox('Select incident type:', incident_opt,  help="Select incident type") 
         collision_type = st.selectbox('Select Collision type:', collision_opt, help="Select Collision type")
 
-        s = st.slider('Floor Area SQM:', min_value=20, max_value=500, value=65, help='Total Estimated space measured in square meters. Minimum value 20 sqm and maximum is 500 sqm.',)
-#         floor = st.selectbox('Select number of floors:', floor_option, help="Estimated number of floors.")
-#         if floor ==3:
-#             floor_no_option = [number for number in range(3,52,3)]
-#         else:
-#             floor_no_option = [number for number in range(5,52,5)]
-#         floor_level = st.selectbox('Select top floor level: ', floor_no_option, index=None, help="Estimated range of floors.", placeholder="Estimated range of floors.")
-    
-#         st.write(' ')
-#         st.write(' ')
-#         button = st.button('Predict Flat Price!')
+       
+        st.write(' ')
+        st.write(' ')
+        button = st.button('Get Insights!')
+        st.write(st.session_state.navigate_to_insights)
     
     
-#     remaining_lease_year = lease_year + 99 - date.today().year if lease_year is not None else None
-#     floor_area_box = transform_single_value(floor_area, lambda_dict['floor_area_lambda'])     if floor_area is not None  else None
-#     town_mapping={'Lim Chu Kang': 1, 'Queenstown': 2, 'Ang Mo Kio': 3, 'Clementi': 4, 'Geylang': 5, 'Bedok': 6, 'Bukit Batok': 7, 'Yishun': 8, 'Toa Payoh': 9, 'Jurong East': 10, 'Central Area': 11, 'Jurong West': 12, 'Kallang/Whampoa': 13, 'Woodlands': 14, 'Hougang': 15, 'Serangoon': 16, 'Marine Parade': 17, 'Bukit Merah': 18, 'Bukit Panjang': 19, 'Tampines': 20, 'Choa Chu Kang': 21, 'Sembawang': 22, 'Pasir Ris': 23, 'Bishan': 24, 'Bukit Timah': 25, 'Sengkang': 26, 'Punggol': 27}
-#     year_mapping = {1990: 1, 1991: 2, 1992: 3, 1993: 4, 1994: 5, 1995: 6, 2002: 7, 2003: 8, 2004: 9, 2001: 10, 2005: 11, 2006: 12, 1999: 13, 2000: 14, 1998: 15, 1996: 16, 2007: 17, 1997: 18, 2008: 19, 2009: 20, 2010: 21, 2019: 22, 2015: 23, 2018: 24, 2011: 25, 2016: 26, 2017: 27, 2014: 28, 2020: 29, 2012: 30, 2013: 31, 2021: 32, 2022: 33, 2023: 34, 2024: 35}
-#     flat_type_mapping = {'1 Room': 1, '2 Room': 2, '3 Room': 3, '4 Room': 4, '5 Room': 5, 'Executive': 6, 'Multi Generation': 7}
-#     flat_model_mapping={'New Generation': 1, 'Standard': 2, 'Simplified': 3, 'Model A2': 4, '2-Room': 5, 'Model A': 6, 'Improved': 7, 'Improved-Maisonette': 8, 'Model A-Maisonette': 9, 'Premium Apartment': 10, 'Adjoined Flat': 11, 'Maisonette': 12, 'Apartment': 13, 'Terrace': 14, 'Multi Generation': 15, 'Premium Maisonette': 16, '3Gen': 17, 'Dbss': 18, 'Premium Apartment Loft': 19, 'Type S1': 20, 'Type S2': 21}
-#     #lease_year_mapping={1969: 1, 1971: 2, 1967: 3, 1968: 4, 1973: 5, 1970: 6, 1972: 7, 1974: 8, 1977: 9, 1980: 10, 1983: 11, 1975: 12, 1981: 13, 1976: 14, 1978: 15, 1979: 16, 1966: 17, 1982: 18, 1985: 19, 1984: 20, 1986: 21, 1987: 22, 1988: 23, 1990: 24, 1989: 25, 1991: 26, 1997: 27, 1998: 28, 1996: 29, 1999: 30, 1994: 31, 1993: 32, 2000: 33, 1995: 34, 1992: 35, 2001: 36, 2002: 37, 2003: 38, 2004: 39, 2012: 40, 2014: 41, 2015: 42, 2005: 43, 2007: 44, 2010: 45, 2013: 46, 2008: 47, 2016: 48, 2009: 49, 2017: 50, 2018: 51, 2019: 52, 2006: 53, 2020: 54, 2011: 55}
-#     # floor_mapping={1:0,2:.5,3: 1,4:1.5, 5: 2,6:2.5}
-#     floor_level_mapping={3: 1, 6: 2, 9: 3, 12: 4, 15: 5, 5: 6, 18: 7, 10: 8, 21: 9, 24: 10, 20: 11, 27: 12, 25: 13, 35: 14, 40: 15, 30: 16, 33: 17, 36: 18, 39: 19, 42: 20, 45: 21, 48: 22, 51: 23}
-#     remaining_lease_year_mapping = {81: 1, 82: 2, 83: 3, 80: 4, 79: 5, 84: 6, 78: 7, 85: 8, 77: 9, 76: 10, 86: 11, 75: 12, 87: 13, 88: 14, 48: 15, 74: 16, 89: 17, 49: 18, 90: 19, 47: 20, 72: 21, 73: 22, 71: 23, 45: 24, 46: 25, 70: 26, 91: 27, 44: 28, 43: 29, 50: 30, 69: 31, 92: 32, 93: 33, 68: 34, 41: 35, 42: 36, 51: 37, 67: 38, 96: 39, 52: 40, 58: 41, 66: 42, 94: 43, 59: 44, 95: 45, 57: 46, 100: 47, 65: 48, 101: 49, 53: 50, 56: 51, 64: 52, 54: 53, 55: 54, 98: 55, 60: 56, 63: 57, 62: 58, 61: 59, 97: 60, 99: 61}
-#     town_median_list = {1: 9.323219299316406,2: 9.260793685913086,3: 9.80174732208252,4: 10.193489074707031,5: 9.80174732208252,6: 10.354616165161133,7: 10.716485977172852,8: 10.615007400512695,9: 9.446247100830078,10: 10.866004943847656,11: 9.446247100830078,12: 11.342947006225586,13: 9.859149932861328,14: 11.29664134979248,15: 11.203139305114746,16: 10.963958740234375,17: 9.916055679321289,18: 9.916055679321289,19: 11.29664134979248,20: 11.342947006225586,21: 11.525309562683105,22: 11.250040054321289,23: 12.172235488891602,24: 11.388961791992188,25: 11.342947006225586,26: 11.203139305114746,27: 10.91515064239502}
-    
-
-    
-
-#     town=town_mapping[town] if town is not None else None
-#     year=year_mapping[date.today().year]
-#     flat_type=flat_type_mapping[flat_type] if flat_type is not None else None
-#     flat_model=flat_model_mapping[flat_model] if flat_model is not None else None
-#    # lease_year=lease_year_mapping[lease_year] if lease_year is not None else None
-#     # floor=floor_mapping[floor] if floor is not None else None
-#     floor_level=floor_level_mapping[floor_level] if floor_level is not None else None
-#     remaining_lease_year=remaining_lease_year_mapping[remaining_lease_year] if remaining_lease_year is not None else None
-    
-    
-#     location_specifics = floor_area_box * town if None not in (floor_area_box, town) else None
-#     #floor_area_year = floor_area_box / remaining_lease_yearj
-    
-#     age = year - lease_year if None not in (year, lease_year) else None
-#     flat_area = flat_type * floor_area_box if None not in (flat_type, floor_area_box) else None
-#     model_area = flat_model * floor_area_box if None not in (flat_model, floor_area_box) else None
-#     town_mean_price = town_median_list[town] if None not in (town, town_median_list) else None
-#     # st.write(lease_year)
-#     floor_area_age = floor_area_box * (2024 - remaining_lease_year) if None not in (floor_area_box, lease_year) else None
-#     floor_weightage = (floor_level -  floor ) * floor_area_box if None not in (floor_level, floor) else None
-    
-    # collisi1on_types = ["Front Collision", "Others", "Rear Collision", "Side Collision"]
-
-# # Simulate user selection (you can replace this with a Streamlit selectbox)
-# selected_collision = st.selectbox("Select Collision Type", collision_types)
-
-# # Initialize an array with zeros
-# one_hot_encoded_array = [0] * len(collision_types)
-
-# # Get the index of the selected collision type
-# selected_index = collision_types.index(selected_collision)
-
-# # Set the value of the selected index to 1
-# one_hot_encoded_array[selected_index] = 1
-
-# # Convert the list to a comma-separated string
-# one_hot_encoded_string = ', '.join(map(str, one_hot_encoded_array))
-
-# # Display the one-hot encoded array as a string
-# st.write("One-Hot Encoded Array:", one_hot_encoded_string)
+#     
 
     sex = {'Male': 1, 'Female':0}
     
     if education:
-        encoded_edu=edu_pickle.transform([education])[0]
+        try:
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                encoded_edu = edu_pickle.transform([education])[0]
+        except Exception as e:
+            st.error(f"Error encoding education: {e}")
+            encoded_edu = None  # or some default value
     
     coll_array = [0] * len(collision_opt)
     selected_index = collision_opt.index(collision_type)
@@ -290,23 +251,51 @@ if selected == "Customer Profile Input":
     hobbies_str = ', '.join(map(str, hobbies_array))
     # st.write(hobbies_str)
     
-    st.write('encoded_edu')
+    st.write(encoded_edu)
     
-J    st.write(data)
-    scaled_data = scale_reg.transform(data)
+    data = np.array([[cust_month,policy_deduc,sex[insured_sex],year,encoded_edu] + [cust_age] + coll_array + hobbies_array + rela_array + inc_array ])
+    st.write(data)
+    # scaled_data = scale_reg.transform(data)
     # st.write(scaled_data)
     
-    
+    st.session_state.data = data
 
-    if button and data is not None:
-        
-        scaled_data = scale_reg.transform(data)
-        # st.write(scaled_data)
-        prediction = XGB_model.predict(scaled_data)
+    if button:
+        if st.session_state.data is not None:
+            try:
+                # Your prediction logic here
+                kmeans_prediction = kmeans.predict(st.session_state.data)
+                # class_prediction = Class_model.predict(st.session_state.data)
+                # reg_prediction = Reg_model.predict(st.session_state.data)
+                
+                # Store predictions in session state
+                st.session_state.prediction = {
+                    'kmeans': kmeans_prediction,
+                    # 'classification': class_prediction,
+                    # 'regression': reg_prediction
+                }
+                
+                # Set flag to navigate to Customer Insights
+                st.session_state.should_navigate = True
+                st.experimental_rerun()
+            except Exception as e:
+                st.error(f"Error: {e}")
+        else:
+            st.warning("Please enter customer data before running the prediction.")
+                
         # st.write(prediction)
-        lambda_val = lambda_dict['resale_price_lambda'] 
-        transformed_predict=reverse_boxcox_transform(prediction, lambda_val) if data is not None else None
-        rounded_prediction = round(transformed_predict[0],2)
-        st.success(f"Based on the input, the Genie's price is,  {rounded_prediction:,.2f}")
-        st.info(f"On average, Genie's predictions are within approximately 10 to 20% of the actual market prices.")
+        # lambda_val = lambda_dict['resale_price_lambda'] 
+        # transformed_predict=reverse_boxcox_transform(prediction, lambda_val) if data is not None else None
+        # rounded_prediction = round(transformed_predict[0],2)
+        # st.success(f"Based on the input, the Genie's price is,  {rounded_prediction:,.2f}")
+        # st.info(f"On average, Genie's predictions are within approximately 10 to 20% of the actual market prices.")
         
+elif selected == "Customer Insights":
+    st.title("Customer Insights")
+    if st.session_state.prediction is not None:
+        st.write("K-means Prediction:", st.session_state.prediction['kmeans'])
+        st.
+        st.write("Classification Prediction:", st.session_state.prediction['classification'])
+        st.write("Regression Prediction:", st.session_state.prediction['regression'])
+    else:
+        st.write("No prediction available. Please run a prediction first.")
